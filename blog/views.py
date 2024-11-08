051d001b8bbc3c728ae222050ea4e754b4a65563
ferditing <tingishaferdinand@gmail.com>
2024-11-08 21:01:40 +0300

    Add post list view, URL routing, and homepage template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')  # Retrieve all posts, ordered by newest first
+    return render(request, 'blog/post_list.html', {'posts': posts})
