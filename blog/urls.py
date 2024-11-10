87eeb26b3dfb17a94b2b52411c612504c4c48867
ferditing <tingishaferdinand@gmail.com>
2024-11-10 18:09:55 +0300

    Added add form editing by form and updated urls and views

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),  # Home page that lists all posts
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/add/', views.add_post, name='add_post'),  # Add post
+    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,  get_object_or_404
+from django.shortcuts import render, redirect,  get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')  # Retrieve all posts, ordered by newest first
@@ -8,3 +9,25 @@ def post_list(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)  # Fetch the post by ID or show a 404 if not found
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def add_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')  # Redirect to the post list after saving
+    else:
+        form = PostForm()
+    return render(request, 'blog/add_post.html', {'form': form})
+
+# View to edit an existing post
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id=post.id)  # Redirect to the post detail page
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/edit_post.html', {'form': form, 'post': post})
